769eacc0f6ef16433b903006a04895134f034309
Rinat273 <bairam0487@gmail.com>
2023-07-11 19:51:09 +0400

    Add post class

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, {adapter: "sqlite3", database: "leprosorium.db"}
+
+class Post < ActiveRecord::Base
+
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
